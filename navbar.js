549456bee86a19b3c82876cc4b696a988cf844b0
Marcin Psioda <marcin.psioda@onet.pl>
2021-03-24 14:58:15 +0100

    Przycisk

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<ul><li><a href="index.html">Strona główna</a></li> '+
+    '<li><a href="kontakt.html">Kontakt</a></li></ul>';
+
+let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
+
+navbarElem.innerHTML = nav;
+
