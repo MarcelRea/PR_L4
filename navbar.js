c4eaaddb4886531870d1d1a94f7345c7e93c273e
Marcin Psioda <marcin.psioda@onet.pl>
2021-03-24 18:52:23 +0100

    DOM

--- navbar.js
+++
@@ -1,7 +0,0 @@
-let nav = '<ul><li><a href="index.html">Strona główna</a></li> '+
-    '<li><a href="kontakt.html">Kontakt</a></li></ul>';
-
-let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
-
-navbarElem.innerHTML = nav;
-
